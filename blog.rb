4dcb44e7020ebc58a77f3bb3cfb5329b5452a0c7
Chrissy Albert <chrissy_albert@yahoo.com>
2016-02-03 15:09:07 -0500

    blog errors

--- blog.rb
+++ blog.rb
@@ -2,40 +2,50 @@ class Blog
 
 	@@total_num_posts = 0
 	@@total_whole_posts_array = []
-	@@total_whole_posts_hash = {}
-	
-	def initialize
-	@@total_num_posts += 1
-	end
-	
-	def self.current_count	
-	return @@total_posts
-	end
-	
+
+	def self.current_count(post)
+   		@@total_whole_posts_array << post
+    	@@total_num_posts += 1
+  	end	
+
+	def self.publish
+    	@@total_whole_posts_array.each do |post|
+      puts "Title:\n #{@title}"
+      puts "Body:\n #{@content}"
+      puts "Publish Date:\n #{@publish_date}"
+    end	
 end
 
 class Blogpost < Blog
 
-	def initialize(title, author, publish_date, content)# I got the idea for this from 'Classes, Objects, Variables'
+	def initialize()# I got the idea for this from 'Classes, Objects, Variables'
 		@title	= title # from http://phrogz.net/programmingruby/frameset.html website
 		@author	= author
 		@publish_date = publish_date
 		@content = content
 	end
-
-	puts "Do you want to create another blog post? Y/N?"
-	answer = gets.chomp.downcase
 	
-	if (answer.downcase == "y")
-		
-		puts "What is the title of your blog post?"
-		@title = gets
+	def self.create
+   		post = self.new
+   		puts "What is the title of your blog post?"
+		@title = gets.chomp
 		puts "What is your name?"
-		@author = gets
+		@author = gets.chomp
 		time = Time
 		@publish_date = time.now.month + time.now.day + time.now.year
 		puts "Please enter your blog post content here."
 		@content = gets
+    	post.save
+		puts "Do you want to create another blog post? Y/N?"
+		create if gets.chomp.downcase == 'y'	
 	end
 
+	def save
+		Blogpost.current_count(self)
+	end
+
+end
+
+Blogpost.create
+Blogpost.publish
 end
