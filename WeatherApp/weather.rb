80f8296e329b060756110d3eeb7c744b09edaa31
Chrissy Albert <chrissy_albert@yahoo.com>
2016-02-11 09:28:25 -0500

    weather.rb works

--- WeatherApp/weather.rb
+++ WeatherApp/weather.rb
@@ -1,15 +1,21 @@
 require 'weatherboy'
 
-def get_weather(location)
+def weather_now(location)
     weatherboy = Weatherboy.new(location)
     w = weatherboy.current.weather
-    puts "The current weather conditions for your area is " + w + "."
+    t = weatherboy.current.temp_f.to_s
+    puts "The current weather conditions for your area is " + w.downcase + " and the temperature is " + t + " degrees Farenheit."
+end
+
+def get_forecast(location)
+    future = Weatherboy.new(location)
+    
 end
 
 puts "Would you like to know the weather for your area? Please enter your 5 digit zipcode."
 user_location = gets.chomp
 
-get_weather(user_location)
-
+weather_now(user_location)
+get_forecast(user_location)
 
 
