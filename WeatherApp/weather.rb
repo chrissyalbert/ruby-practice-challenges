4a846c6c348781f2139eee2dd67cd3720b42e99f
Chrissy Albert <chrissy_albert@yahoo.com>
2016-02-12 14:34:01 -0500

    final changes

--- WeatherApp/weather.rb
+++ WeatherApp/weather.rb
@@ -5,22 +5,23 @@ def weather_now(location)
     w = weatherboy.current.weather
     t = weatherboy.current.temp_f.to_s
     puts "The current weather conditions for your area is " + w.downcase + " and the temperature is " + t + " degrees Farenheit."
+    puts " "
+end
+
+def weather_next(location)
+    forecast = Weatherboy.new(location).forecasts
+    require 'date'
+    today = Date.today
+    date = [today, today +1, today +2, today +3, today +4]
+    date.zip(forecast).each do |date, forecast|
+        puts (date.strftime('%A') + ", " + date.strftime('%b') + " " + date.strftime('%d') + ", " + date.strftime('%Y')) 
+        puts "The weather conditions will be " + forecast.conditions.downcase + ", and the high will be " + forecast.high_f.to_s + " with a low of " + forecast.low_f.to_s + "."
+        puts " "
+    end
 end
 
 puts "Would you like to know the weather for your area? Please enter your 5 digit zipcode."
 location = gets.chomp
-
-def weather_next(location)
-    forecast = Weatherboy.new(location).forecasts
-    
-    forecast.each do |forecast|
-        day = forecast.title
-        cond = forecast.conditions
-        high = forecast.high_f.to_s
-        low = forecast.low_f.to_s
-        puts "On #{day} the weather will be #{cond.downcase} with a high temperature of #{high} and a low of #{low}."
-    end
-end
 
 weather_now(location)
 weather_next(location)
