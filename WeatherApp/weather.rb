4baa2c6adb0d6b1c17925b10f9848eee21dd14f1
Chrissy Albert <chrissy_albert@yahoo.com>
2016-02-12 13:59:30 -0500

    works, but day is wrong

--- WeatherApp/weather.rb
+++ WeatherApp/weather.rb
@@ -7,15 +7,20 @@ def weather_now(location)
     puts "The current weather conditions for your area is " + w.downcase + " and the temperature is " + t + " degrees Farenheit."
 end
 
-def get_forecast(location)
-    future = Weatherboy.new(location)
+puts "Would you like to know the weather for your area? Please enter your 5 digit zipcode."
+location = gets.chomp
+
+def weather_next(location)
+    forecast = Weatherboy.new(location).forecasts
     
+    forecast.each do |forecast|
+        day = forecast.title
+        cond = forecast.conditions
+        high = forecast.high_f.to_s
+        low = forecast.low_f.to_s
+        puts "On #{day} the weather will be #{cond.downcase} with a high temperature of #{high} and a low of #{low}."
+    end
 end
 
-puts "Would you like to know the weather for your area? Please enter your 5 digit zipcode."
-user_location = gets.chomp
-
-weather_now(user_location)
-get_forecast(user_location)
-
-
+weather_now(location)
+weather_next(location)
